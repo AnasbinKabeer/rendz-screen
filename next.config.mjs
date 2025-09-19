76071541ff741daa9bc29b52ae31fdeb1b5d4f95
AnasbinKabeer <hafizapk007@gmail.com>
2025-09-19 16:01:22 +0530

    next condig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+  output: "export",  // generates /out folder
+  images: {
+    unoptimized: true, // make <Image /> work without optimization server
+  },
+  assetPrefix: "./", // ✅ ensures images/static load from relative path
 };
 
 export default nextConfig;
